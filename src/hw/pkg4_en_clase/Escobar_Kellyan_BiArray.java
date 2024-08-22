df7db5e833b24e29cd65d31990eb09e03a4779f0
laraj <laraj@DESKTOP-CDBJIFN>
2024-08-22 15:55:09 -0600

    trabajo en clase 2

--- src/hw/pkg4_en_clase/Escobar_Kellyan_BiArray.java
+++ src/hw/pkg4_en_clase/Escobar_Kellyan_BiArray.java
@@ -1,39 +1,76 @@
 package hw.pkg4_en_clase;
 import java.util.Scanner;
 import java.util.Random;
-
+import java.util.InputMismatchException;
 public class Escobar_Kellyan_BiArray {
     public static void main(String[] args) {
         Scanner lea=new Scanner (System.in);
-        Random random = new Random();
+        Random random=new Random();
         
-        System.out.println("Ingrese los numeros de n x n: ");
-        int numero = lea.nextInt();
-        int matriz[][] = new int[numero][numero];
-
-        for (int n1 = 0; n1 < numero; n1++) {
-            for (int n2 = 0; n2 < numero; n2++) {
-                matriz[n1][n2] = random.nextInt(90) + 10;
+        int numero=0;
+        boolean valido=false;
+        while (!valido) {
+            try {
+                System.out.println("Ingrese los numeros de n x n: ");
+                numero=lea.nextInt();
+                valido=true;
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Por favor ingrese un numero.");
+                lea.next(); 
             }
         }
+        
+        if (valido) {
+            int matriz[][] =new int[numero][numero];
 
-        for (int n1 = 0; n1 < numero; n1++) {
-            for (int n2 = 0; n2 < numero; n2++) {
-                System.out.print(matriz[n1][n2] + "\t");
+            for (int fila=0; fila<numero; fila++) {
+                for (int columna=0; columna<numero; columna++) {
+                    matriz[fila][columna]=random.nextInt(90) + 10;
+                    System.out.println();
+                }
             }
-            System.out.println();
-        }
 
-        int suma1 = 0;
-        for (int n1 = 0; n1 < numero; n1++) {
-            suma1 += matriz[n1][n1];
-        }
-        System.out.println("\nSuma diagonal principal: " + suma1);
+            for (int fila=0; fila<numero; fila++) {
+                for (int columna=0; columna<numero; columna++) {
+                    System.out.print(matriz[fila][columna] + "\t");
+                }
+                System.out.println();
+            }
 
-        int suma2 = 0;
-        for (int n1 = 0; n1 < numero; n1++) {
-            suma2 += matriz[n1][numero - 1 - n1];
+            int suma1=0;
+            for (int fila=0; fila<numero; fila++) {
+                suma1 +=matriz[fila][fila];
+            }
+            
+            System.out.println("\n1. Suma de la Diagonal Principal:");
+            for (int fila=0; fila < numero; fila++) {
+                for (int columna=0; columna<numero; columna++) {
+                    if (fila==columna) {
+                        System.out.print(matriz[fila][columna] + "\t");
+                    } else {
+                        System.out.print("\t");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println("Suma principal: " + suma1);
+
+            int suma2=0;
+            for (int fila=0; fila<numero; fila++) {
+                suma2 +=matriz[fila][numero - 1 - fila];
+            }
+            System.out.println("\n 2. Suma de la Diagonal Secundaria:");
+            for (int fila=0; fila<numero; fila++) {
+                for (int columna = 0; columna<numero; columna++) {
+                    if (fila==numero - 1 - columna) {
+                        System.out.print(matriz[fila][columna] + "\t");
+                    } else {
+                        System.out.print("\t");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println("Suma secundaria: " + suma2);
         }
-        System.out.println("Suma diagonal secundaria: " + suma2);
     }
 }
